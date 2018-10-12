4131d929bbe6f46f851ade0a204d2549a211890a
Rick Blommers <rick@blommersit.nl>
2018-10-12 15:18:51 +0200

    Fix optional

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Product.class_eval do
-  has_one :slide, :dependent => :destroy, optional: true
+  has_one :slide, :dependent => :destroy
   after_update :destroy_slide_if_deleted
   def destroy_slide_if_deleted
     slide.update_attributes(:published => false) if slide && deleted_at

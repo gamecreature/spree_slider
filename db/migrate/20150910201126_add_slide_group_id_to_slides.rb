742919bd422f597fb2cdbab1b1c704b70e461c71
Rick Blommers <rick@blommersit.nl>
2018-10-12 15:12:01 +0200

    Fixed migrations

--- db/migrate/20150910201126_add_slide_group_id_to_slides.rb
+++ db/migrate/20150910201126_add_slide_group_id_to_slides.rb
@@ -1,4 +1,4 @@
-class AddSlideGroupIdToSlides < ActiveRecord::Migration
+class AddSlideGroupIdToSlides < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_slides, :slide_group_id, :integer, :null => false, :default => 0
   end
